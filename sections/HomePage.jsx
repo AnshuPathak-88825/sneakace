6c35ecd0d2602669c99e593eb0e5d1f039c74f0f
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-12-02 11:06:40 +0530

    Navbar animation

--- sections/HomePage.jsx
+++ sections/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import TransitionEffect from "../components/TransitionEffect";
 const HomePage = () => {
   return (
-    <div className="w-full lg:h-[80vh] p-10">
+    <div className="w-full lg:h-[91vh] p-10">
       <TransitionEffect />
       {/* <Image src="https://giphy.com/clips/southpark-south-park-sneakers-adidas-x-StGHM8jxjXthSs2kHx" width={700} height={700} /> */}
     </div>
--- sections/Navbar.jsx
+++ sections/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import { useEffect } from "react";
+
 import logo from "../public/assets/images/logo.svg";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
@@ -16,31 +18,53 @@ const Navbar = () => {
   const handleChange = () => {
     setNav(!nav);
   };
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setIsScrolled(scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
+    <div
+            className={`${
+              isScrolled ? "translate-y-0" : "translate-y-[-5rem]"
+            } transition-all duration-1000 ease-in-out fixed top-0 left-0 right-0 z-10`}
+          >
       <div className="w-full bg-gradient-to-r from-background via-secondary/80 to-background h-20 sticky top-0 z-[999]">
         <div className="container mx-auto flex items-center justify-between w-full h-full px-4 lg:px-16">
-          <Link href="/">
-            <div className="flex items-center gap-3">
-              <Image src={logo} alt="logo" width={100} height={100} />
-              <h1 className="hidden lg:block font-bold">SNEAKACE</h1>
-            </div>
-          </Link>
+          
+            <Link href="/">
+              <div className="flex items-center gap-3">
+                <Image src={logo} alt="logo" width={100} height={100} />
+                <h1 className="hidden lg:block font-bold">SNEAKACE</h1>
+              </div>
+            </Link>
 
-          <div className="hidden lg:flex items-center gap-3">
-            <div className="rounded-full border p-2 cursor-pointer">
-              <BsFillPersonFill size={30} />
+            <div className="hidden lg:flex items-center gap-3">
+              <div className="rounded-full border p-2 cursor-pointer">
+                <BsFillPersonFill size={30} />
+              </div>
+              <div className="rounded-full border p-2 cursor-pointer hover:bg-secondary/50 duration-300">
+                <Link href="/login">
+                  <button className="w-[120px] text-xl">Login</button>
+                </Link>
+              </div>
             </div>
-            <div className="rounded-full border p-2 cursor-pointer hover:bg-secondary/50 duration-300">
-              <Link href="/login">
-                <button className="w-[120px] text-xl">Login</button>
-              </Link>
-            </div>
-          </div>
-
           <div
             className="flex lg:hidden rounded-full border p-2"
-            onClick={handleChange}>
+            onClick={handleChange}
+          >
             {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
           </div>
 
@@ -49,7 +73,8 @@ const Navbar = () => {
               nav
                 ? "fixed left-0 top-0 w-[70%] sm:w-[60%] md:w-[45%] h-screen bg-background p-10 ease-in duration-500"
                 : "fixed left-[-100%] top-0 h-screen p-10 ease-in duration-700"
-            }>
+            }
+          >
             <Link href="/" onClick={handleChange}>
               <h2 className="text-primary">SNEAKACE</h2>
             </Link>
@@ -58,10 +83,12 @@ const Navbar = () => {
                 <ul
                   key={index}
                   className="text-xl text-primary"
-                  onClick={handleChange}>
+                  onClick={handleChange}
+                >
                   <Link
                     href={menu.path}
-                    className="hover:text-secondary duration-200">
+                    className="hover:text-secondary duration-200"
+                  >
                     <li>{menu.label}</li>
                   </Link>
                 </ul>
@@ -74,6 +101,7 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
