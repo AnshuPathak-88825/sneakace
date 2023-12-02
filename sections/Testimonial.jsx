35ddfca3ea5914d3f31ba9cef91c4ac583170f2d
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-12-02 15:42:57 +0530

    review section changed

--- sections/Testimonial.jsx
+++ sections/Testimonial.jsx
@@ -19,7 +19,8 @@ const Testimonial = () => {
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, natus!",
       name: "Mr. Sakshaat",
       date: "14th October, 2023",
-    },
+    }
+    ,
     {
       photo: "/assets/images/avator1.png",
       description:
@@ -37,7 +38,7 @@ const Testimonial = () => {
     {
       photo: "/assets/images/avator1.png",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate atque labore natus vel, provident aliquam illo dolor laborum repellat enim amet pariatur consequuntur est at culpa aperiam eum facere accusamus.",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, natus!",
       name: "Mr. Sakshaat",
       date: "14th October, 2023",
     },
@@ -61,7 +62,7 @@ const Testimonial = () => {
     <section className="w-full p-3 mt-10 bg-primary rounded-tl-[60px] lg:rounded-tl-[245px] rounded-tr-[60px] lg:rounded-tr-[243px]">
 
       <div className="flex items-center justify-center">
-      <h2 className="text-secondary">WHAT OUR CUSTOMERS SAY</h2>
+        <h2 className="text-secondary">WHAT OUR CUSTOMERS SAY</h2>
       </div>
       <div className="relative flex items-center justify-center bg-[#FEF9EC] rounded-tl-[60px] lg:rounded-tl-[245px] rounded-tr-[60px] lg:rounded-tr-[243px] p-20 mt-10">
         <MdChevronLeft
@@ -74,26 +75,24 @@ const Testimonial = () => {
           className="w-full h-full overflow-x-scroll overflow-hidden scroll whitespace-nowrap scroll-smooth scrollbar-hide">
           <div className="flex gap-28">
             {testimonials.map((testimonial, index) => (
-              <div key={index} className="flex flex-col items-center lg:w-[250px]">
-                <Image
-                  src={testimonial.photo}
-                  width={200}
-                  height={200}
-                  alt={testimonial.name}
-                  className="rounded-full"
-                />
-                <p className="text-xl lg:text-2xl text-background ">
-                  {testimonial.name}
-                </p>
-                <p className="text-xl lg:text-2xl text-background ">
-                  {testimonial.date}
-                </p>
-                {/* <p className="text-lg text-center text-background">
-                  {testimonial.description}
-                </p> */}
-              </div>
+            <div key={index} className="flex flex-col items-center lg:w-[600px]  h-[300px]  ">
+            <Image
+              src={testimonial.photo}
+              width={200}
+              height={200}
+              alt={testimonial.name}
+              className="rounded-full"
+            />
+            <p className="text-xl lg:text-2xl text-background">{testimonial.name}</p>
+            <p className="text-lg lg:text-xl text-background">{testimonial.date}</p>
+            <p className="text-base	 text-center text-opacity-60	text-background whitespace-normal">
+              {testimonial.description}
+            </p>
+          </div>
+          
             ))}
           </div>
+
         </div>
 
         <MdChevronRight
