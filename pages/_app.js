f34394c057ef015ba74f777f950cf21837804b90
Ved Prakash <ved78142@gmail.com>
2023-12-18 00:08:17 +0530

    fixing Auth and some api work

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { AuthContextProvider } from "../context/AuthContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthContextProvider>
   );
 }
